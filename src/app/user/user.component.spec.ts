441a19ac8da24f1dbda7b6e8dc686b673600beba
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-21 16:03:59 +0530

    service attached

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,26 +1,24 @@
-
-
 import { TestBed } from '@angular/core/testing';
-import {UserComponent} from './user.component';
-
+import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 describe('testing user component', () => {
-
-  beforeEach(()=> {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UserComponent]
-    })
-  })
+      declarations: [UserComponent],
+    });
+  });
 
-it('An app should be created with user component inside it', () => {
+  it('An app should be created with user component inside it', () => {
+    let fixture = TestBed.createComponent(UserComponent); //arrange
+    let app = fixture.debugElement.componentInstance; //action
+    expect(app).toBeTruthy(); //assert
+  });
 
-  let fixture = TestBed.createComponent(UserComponent);//arrange
-  let app = fixture.debugElement.componentInstance;//action
-  expect(app).toBeTruthy() //assert
+  it('should update the user value from a service', () => {
+    let fixture = TestBed.createComponent(UserComponent); //arrange
+    let app = fixture.debugElement.componentInstance; //action
+    let userService = fixture.debugElement.injector.get(UserService);
+    
 
+  });
 });
-
-
-
- })
-
-
